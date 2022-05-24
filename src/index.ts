31d890123e1488255225d73bfc5e502cad49b09d
James Pacheco <jamesrp13@gmail.com>
2022-05-23 18:50:08 -0600

    Initial Commit

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import { initializeKeypair } from './initializeKeypair'
+import * as web3 from '@solana/web3.js'
+
+async function main() {
+    const connection = new web3.Connection(web3.clusterApiUrl('devnet'))
+    const signer = await initializeKeypair(connection)
+    
+}
+
+main().then(() => {
+    console.log('Finished successfully')
+    process.exit(0)
+}).catch(error => {
+    console.log(error)
+    process.exit(1)
+})
